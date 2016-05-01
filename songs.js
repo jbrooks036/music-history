51a77e2c43b0f0c48ad89ba084a1e4c0f4d0ebb2
jbrooks036 <joannbrooks036@gmail.com>
2016-05-01 13:53:11 -0500

    injecting song info from .js into DOM

--- songs.js
+++ songs.js
@@ -1,20 +1,29 @@
 var songs = [];
 
-// songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-// songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-// songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-// songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-// songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
-console.log(songs);
-
-// add songs to end of songs array
+// add song objects to songs array
 songs.push({title:"Clinch Mountain Backstep", artist: "Ralph Stanley", album: "Best of the Best"});
-console.log(songs);
-
 songs.push({title:"Shady Grove", artist: "The Ebony Hillbillies", album: "Barefoot and Flying"});
-console.log(songs);
-
 songs.push({title:"Caught in the Crossfire", artist: "Rhonda Vincent", album: "One Step Ahead"});
-console.log(songs);
+console.log(songs); // confirm they're in the array
 
 // {Song name} by {Artist} on the album {Album}
+var song1Title = document.getElementById("song1-title");
+song1Title.innerHTML = songs[0].title;
+var song1Artist = document.getElementById("song1-artist");
+song1Artist.innerHTML = songs[0].artist;
+var song1Album = document.getElementById("song1-album");
+song1Album.innerHTML = songs[0].album;
+
+var song2Title = document.getElementById("song2-title");
+song2Title.innerHTML = songs[1].title;
+var song2Artist = document.getElementById("song2-artist");
+song2Artist.innerHTML = songs[1].artist;
+var song2Album = document.getElementById("song2-album");
+song2Album.innerHTML = songs[1].album;
+
+var song3Title = document.getElementById("song3-title");
+song3Title.innerHTML = songs[2].title;
+var song3Artist = document.getElementById("song3-artist");
+song3Artist.innerHTML = songs[2].artist;
+var song3Album = document.getElementById("song3-album");
+song3Album.innerHTML = songs[2].album;
