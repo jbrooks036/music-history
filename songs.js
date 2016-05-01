bc4d45a26ed71ad35db5a462a045c0900a7c5ad5
jbrooks036 <joannbrooks036@gmail.com>
2016-05-01 12:39:37 -0500

    changed song type from string to object

--- songs.js
+++ songs.js
@@ -1,17 +1,20 @@
 var songs = [];
 
-songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+// songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
+// songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
+// songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+// songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
+// songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
 console.log(songs);
 
-// add song to beginning of songs array
-songs.unshift("Clinch Mountain Backstep > by Ralph Stanley on the album Best of the Best");
+// add songs to end of songs array
+songs.push({title:"Clinch Mountain Backstep", artist: "Ralph Stanley", album: "Best of the Best"});
 console.log(songs);
 
-// add song to end of songs array
-songs.push("Shady Grove > by The Ebony Hillbillies on the album Barefoot and Flying");
+songs.push({title:"Shady Grove", artist: "The Ebony Hillbillies", album: "Barefoot and Flying"});
 console.log(songs);
 
+songs.push({title:"Caught in the Crossfire", artist: "Rhonda Vincent", album: "One Step Ahead"});
+console.log(songs);
+
+// {Song name} by {Artist} on the album {Album}
